789382d88fb5f45ccc725022725518344a451498
Adrian zen <1781935376@qq.com>
2024-12-14 21:17:57 +0800

    初始化goframe项目

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+
+	"github.com/gogf/gf/v2"
+)
+
+type HelloReq struct {
+}
+
+func main() {
+	s := g.Server()
+	s.BindHandler("/", func(r *ghttp.Request) {
+		r.Response.Writef("hello %s!,your age is %d", r.Get("name", "unknow").String(), r.Get("age", 18).Int())
+	})
+	s.SetPort(8080)
+	s.Run()
+	fmt.Println("Hello Gofraemwork!:", gf.VERSION)
+}
