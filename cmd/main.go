e32798abfc2486fa304b92dcb71df8e29427413d
Adrian zen <1781935376@qq.com>
2024-12-14 22:24:36 +0800

    使用规范路由

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
+	"context"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
 type HelloReq struct {
-	Name string // 姓名
-	Age  int    // 年龄
+	g.Meta `path:"/" method:"get"`
+	Name   string `v:"required" dc:"姓名"`
+	Age    int    `v:"required" dc:"年龄"`
+}
+type HelloRes struct{}
+
+type Hello struct{}
+
+func (Hello) Say(ctx context.Context, req *HelloReq) (res *HelloRes, err error) {
+	r := g.RequestFromCtx(ctx)
+	r.Response.Writef("hello %s! your age is %d",
+		req.Name, req.Age)
+	return
 }
 
 func main() {
 	s := g.Server()
-	s.BindHandler("/", func(r *ghttp.Request) {
-		var req HelloReq
-		if err := r.Parse(&req); err != nil {
-			r.Response.Write(err.Error())
-			return
-		}
-		if req.Name == "" {
-			r.Response.Write("name should not be empty")
-			return
-		}
-		if req.Age <= 0 {
-			r.Response.Write("invalid age value")
-			return
-		}
-		r.Response.Writef("hello %s! your age is %d", req.Name, req.Age)
+	s.Group("/", func(group *ghttp.RouterGroup) {
+		group.Bind(new(Hello))
 	})
 	s.SetPort(8080)
 	s.Run()
